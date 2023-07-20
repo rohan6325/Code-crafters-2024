d5ae5e44d01446276038838503ffc266438bb4a0
Rohan Sharma <sharma.rs@somaiya.edu>
2023-07-20 22:38:28 +0530

    initial commit

---
+++ project/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+posts =[
+    {
+        'author': 'Rohan',
+        'title':'day 1',
+        'content':'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+        'date':'today' 
+        },
+         {
+        'author': 'Rahul',
+        'title':'day 2',
+        'content':'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+        'date':'yesterday' 
+        },
+         {
+        'author': 'Omika',
+        'title':'day 3',
+        'content':'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available',
+        'date':'2 days ago' 
+        }
+]
+def home (request):
+    context ={
+        'posts':posts
+    }
+    return render(request, 'home.html',context)
+def about (request):
+    return render(request, 'index.html')
