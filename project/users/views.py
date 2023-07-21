418c1530cbb805475414948a6760812a20c3a9c6
Rohan Sharma <sharma.rs@somaiya.edu>
2023-07-22 00:57:31 +0530

    login complete

--- project/users/views.py
+++ project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 # Create your views here.
@@ -9,8 +10,11 @@ def register(request):
         if form.is_valid():
             form.save()
             Username= form.cleaned_data.get('username')
-            messages.success(request,f'Account is created for username :{Username}!')
-            return redirect('home')
+            messages.success(request,f'Your Account is created for username :{Username}!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html',{'form':form})
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
