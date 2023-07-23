6ee1860d5f14cd8dc3c8932773d1efc7344c88fd
Rohan Sharma <sharma.rs@somaiya.edu>
2023-07-23 14:10:02 +0530

    profile pics

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings 
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,3 +31,5 @@ urlpatterns = [
     path('profile',users_views.profile, name='profile')
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
