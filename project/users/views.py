bd329fd9744bb671b480a9287c6205be428f8600
Rohan Sharma <sharma.rs@somaiya.edu>
2023-07-24 19:40:37 +0530

    update

--- project/users/views.py
+++ project/users/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm , UserUpdateForm , ProfileUpdateForm
 # Create your views here.
 def register(request):
     if request.method =='POST':
@@ -17,4 +16,13 @@ def register(request):
     return render(request, 'register.html',{'form':form})
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+    
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'profile.html', context)
+
+
