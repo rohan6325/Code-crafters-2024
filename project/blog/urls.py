263d170998938be0ad8242b7a9def47b2cba679e
Rohan Sharma <sharma.rs@somaiya.edu>
2023-07-25 00:35:23 +0530

    update-delete buttons

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", PostListView.as_view(), name="home"),
       path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
       path("posts/new/", PostCreateView.as_view(), name="post-create"),
-       path("post/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
+       path("postupdate/<int:pk>/", PostUpdateView.as_view(), name="post-update"),
     path("about",views.about , name='about'),
     path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete")
 ]
