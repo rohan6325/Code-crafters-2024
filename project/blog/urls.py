78398ee2c666ae84085213d9d35c412da2e18168
Rohan Sharma <sharma.rs@somaiya.edu>
2023-07-26 22:33:04 +0530

    user post complete

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from .views import PostListView , PostDetailView , PostCreateView ,PostUpdateView ,PostDeleteView 
+from .views import PostListView , PostDetailView , PostCreateView ,PostUpdateView ,PostDeleteView , UserPostListView 
 urlpatterns = [
     path("", PostListView.as_view(), name="home"),
+    path("userpost/<str:username>", UserPostListView.as_view(), name="User-posts"),
       path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
       path("posts/new/", PostCreateView.as_view(), name="post-create"),
        path("postupdate/<int:pk>/", PostUpdateView.as_view(), name="post-update"),
--- project/blog/views.py
+++ project/blog/views.py
@@ -11,16 +11,16 @@ class PostListView(ListView):
     template_name ='home.html'
     context_object_name ='posts'
     ordering = ['-date']
-    paginate_by = 5 
+    paginate_by = 4 
     
 class UserPostListView(ListView):
     model = post
     template_name ='User_posts.html'
     context_object_name ='posts'
-    paginate_by = 5
-    def get_query_set(self):
+    paginate_by = 4
+    def get_queryset(self):
         user = get_object_or_404(User , username = self.kwargs.get('username'))
-        return  post.object.filter(author=user).order_by('-date')
+        return  post.objects.filter(author=user).order_by('-date')
          
     
 class PostDetailView(DetailView):
